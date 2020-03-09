fd9e9800b662831a9788db76875aac4ba88fa0b1
Domenico Cascone <dcascone84@gmail.com>
2020-03-09 23:17:37 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,6 +6,7 @@ def batch_badge_creator(attendees)
     attendees.each do |name|
       new_array = []
       new_array.push("Hello, my name is #{name}.")
+      new_array
     end
     return new_array
 end
