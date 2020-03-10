18c60d35f9026a3f0e86a98a880fd79e457c3951
Domenico Cascone <dcascone84@gmail.com>
2020-03-10 00:19:16 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,19 +3,19 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-  new_array = []
+  name_of_attendee = []
     attendees.each do |name|
       new_array.push("Hello, my name is #{name}.")
     end
-  return new_array
+  return name_of_attendee
 end
 
 def assign_rooms(attendees)
-  new_array = []
+  name_and_room_number = []
   attendees.each.with_index(1) do |name, index|
     new_array.push("Hello, #{name}! You'll be assigned to room #{index}!")
   end
-  return new_array
+  return name_and_room_number
 end
 
 def printer(assign_rooms)
